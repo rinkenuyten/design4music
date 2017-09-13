171c340881a57fa021d5122937306dd41999afa8
RinkeNuytenLaptop <rinke.nuyten@hotmail.com>
2017-09-13 12:39:58 +0200

    Update on movement of points

--- EffenaarRunner/Assets/move.cs
+++ EffenaarRunner/Assets/move.cs
@@ -6,11 +6,14 @@ public class move : MonoBehaviour {
     public float speed;
     private float initialheight;
     public AnimationCurve myCurve;
+    private float timeStarter;
 
     // Use this for initialization
     void Start () {
         initialheight = this.transform.position.y;
-	}
+        timeStarter = Random.Range(-1.0f, 2.0f);
+        Debug.Log(timeStarter);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,9 +21,9 @@ public class move : MonoBehaviour {
         horizontalposition.x -= speed;
         this.transform.position = horizontalposition;
 
-        transform.position = new Vector3(transform.position.x, initialheight + myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+        transform.position = new Vector3(transform.position.x, initialheight + myCurve.Evaluate(((Time.time + timeStarter) % myCurve.length)), transform.position.z);
 
-        if (this.transform.position.x < -15)
+        if (this.transform.position.x < -20)
         {
             Destroy(this.transform.gameObject);
         }
